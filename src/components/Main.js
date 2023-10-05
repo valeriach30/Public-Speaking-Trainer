023d39c6f1d0a8b0e3e6735e1011a2e6b4ddacdf
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-10-05 11:23:39 -0600

    Add video upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../styles/landing.css';
 import Header from './Header';
+import VideoInput from "./VideoInput";
+
 function Main() {
     return (
         <div className="landing-page">
             <Header />
             <div className="content">
-                <h1>Welcome to Public Speaking Trainer AI</h1>
+                <h1 className='title'>Welcome to Public Speaking Trainer AI</h1>
             </div>
+            <h2>&#127916; Upload your video to start!</h2>
+            <VideoInput width={800} height={800} />
+
+            <h2>&#9197; Start analysis!</h2>
         </div>
     );
 }
