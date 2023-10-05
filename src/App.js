cb6cc993dd68065bb5b22b440d3b7da757aea18c
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-10-04 21:24:42 -0600

    Change landing page
    Added styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/landing.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola Mundo</h1>
+    <div className="landing-page">
+      <header>
+        <div className="container">
+          <a href="#" className="logo">Speech <b>Crafters</b></a>
+          <ul className="links">
+            <li>Home</li>
+            <li>Practice</li>
+          </ul>
+        </div>
       </header>
+      <div className="content">
+        <div className="container">
+          <div className="info">
+            <h1>Public Speaking Trainer</h1>
+            <p>Our program empowers you to speak with confidence, captivate any audience, and leave a lasting impression. Whether you're new to public speaking or looking to refine your skills</p>
+            <button>Start Now!</button>
+          </div>
+          <div className="image">
+            <img src="https://img.freepik.com/premium-vector/speech-man-podium-illustration_111797-93.jpg" alt="Description of the image" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
